d93819a821ba4e62928987469124c3e7149cb0a3
Bhavin Shah <bhavinshah120986@gmail.com>
2026-07-01 14:45:48 +0530

    Moving UI-Playground to its own repo

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# ui-playground
+# UI Playground
+
+## Useful Data
+### Client Side Tooling
+https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Client-side_tools/Overview
+#### TODO - expand on this topic
+Environment — Tools that help you set up your development environment, such as installing and running other tools.
+Safety net — Tools that are useful during your code development.
+Transformation — Tools that transform code in some way, e.g., turning an intermediate language into JavaScript that a browser can understand.
+Post-development — Tools that are useful after you have written your code, such as testing and deployment tools.
+
+### React Cheat Sheet
+- Hooks: https://react.dev/reference/react/hooks
+- Server Components: https://react.dev/reference/rsc/server-components
+- Recommended Build Tools: https://react.dev/learn/build-a-react-app-from-scratch
+
+
+### CSS Cheat Sheet
+- Flexbox: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+  
---
+++ react-tic-tac-toe/src/App.tsx
@@ -0,0 +1,124 @@
+import { useState } from 'react';
+
+function Square({ text, handleClick, winningCombination, id }) {
+    return <button style={{ backgroundColor: winningCombination.includes(id) ? 'lightgreen' : 'white' }} className="square" onClick={handleClick}>{text}</button>;
+}
+
+function HistoryStep({ text, handleClick}) {
+    return <button onClick={handleClick}>{text}</button>;
+}
+
+function GameBoard({ boardValues, handleClick, winningCombination }) {
+    return (
+        <>
+            <div className="board-row">
+                <Square winningCombination={winningCombination} id={0} key="0" text={boardValues[0]} handleClick={() => handleClick(0)} />
+                <Square winningCombination={winningCombination} id={1} key="1" text={boardValues[1]} handleClick={() => handleClick(1)} />
+                <Square winningCombination={winningCombination} id={2} key="2" text={boardValues[2]} handleClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square winningCombination={winningCombination} id={3} key="3" text={boardValues[3]} handleClick={() => handleClick(3)} />
+                <Square winningCombination={winningCombination} id={4} key="4" text={boardValues[4]} handleClick={() => handleClick(4)} />
+                <Square winningCombination={winningCombination} id={5} key="5" text={boardValues[5]} handleClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square winningCombination={winningCombination} id={6} key="6" text={boardValues[6]} handleClick={() => handleClick(6)} />
+                <Square winningCombination={winningCombination} id={7} key="7" text={boardValues[7]} handleClick={() => handleClick(7)} />
+                <Square winningCombination={winningCombination} id={8} key="8" text={boardValues[8]} handleClick={() => handleClick(8)} />
+            </div>
+        </>
+    );
+}
+
+export default function App() {
+    const [boardValues, setBoardValues] = useState(['', '', '', '', '', '', '', '', '']);
+    const [nextPlayer, setNextPlayer] = useState("X");
+    const [winner, setWinner] = useState("");
+    // 0th place is player who played current step, rest are board values after playing current step
+    const [history, setHistory] = useState([['X', '', '', '', '', '', '', '', '', '']]);
+    const [winningCombination, setWinningCombination] = useState([]);
+
+    const checkWinnner = (board: string[]) => {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (const combination in winningCombinations) {
+            const indices = winningCombinations[combination];
+            if (board[indices[0]] !== '' && board[indices[0]] === board[indices[1]] && board[indices[1]] === board[indices[2]]) {
+                setWinner(nextPlayer);
+                setWinningCombination([...indices]);
+                return;
+            }
+        }
+    }
+
+    const handleClick = (id: number) => {
+        // If square already clicked, ignore
+        if (winner !== '' || boardValues[id] !== '') {
+            return;
+        }
+
+        // Set the current player's icon in the clicked square on the board
+        let newBoardValues = [...boardValues];
+        newBoardValues[id] = nextPlayer;
+        setBoardValues(newBoardValues);
+
+        // Add the new board values to the history
+        let newHistory = [...history];
+        const currentPlayer = nextPlayer;
+        newHistory.push([currentPlayer, ...newBoardValues]);
+        setHistory(newHistory);
+
+        // Check Winner
+        checkWinnner(newBoardValues);
+
+        // Switch player
+        setNextPlayer(nextPlayer === "X" ? "O" : "X");
+    }
+
+    const goToStep = (step: number) => {
+        // Update board values to the selected step in history
+        let newBoardValues = [...boardValues];
+        newBoardValues = history[step].slice(1);
+        setBoardValues(newBoardValues);
+        // Delete later steps from history
+        setHistory(history.slice(0, step + 1));
+        // Update next player to opposite of the player who played in the selected step
+        setNextPlayer(history[step][0] === "X" ? "O" : "X");
+        // If restarting game
+        if (step === 0) {
+            setNextPlayer("X");
+        }
+        // If user moves back in history, reset winner
+        setWinner("");
+        setWinningCombination([]);
+
+    }
+
+    const historySteps = history.map((_, index) => {
+        const text = index === 0 ? 'Restart Game' : `Go to Step ${index}`;
+        const isLatestStep = index === history.length - 1;
+        return (
+            <div key={index} style={{ marginBottom: '10px' }}>
+                {isLatestStep ? `You are at step ${index}` : <HistoryStep text={text} handleClick={() => goToStep(index)} />}
+            </div>
+        );
+    });
+
+    return (
+        <>
+            <h1>Tic Tac Toe</h1>
+            {winner && <h2>Winner: {winner}</h2>}
+            {!winner && <h2>Next Player: {nextPlayer}</h2>}
+            <GameBoard boardValues={boardValues} handleClick={handleClick} winningCombination={winningCombination} />
+            <div style={{ marginTop: '20px' }}>{ historySteps }</div>
+        </>
+    );
+}
